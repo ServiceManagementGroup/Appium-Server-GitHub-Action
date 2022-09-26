36bed8b9798175decc82308df31a3db3fe7a1e63
Andrew Vavricek <avavricek@gmail.com>
2022-09-26 16:30:44 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,6 @@ echo "Install and Running Appium Server as a Background process"
 sudo npm install -g appium --unsafe-perm=true --allow-root
 appium -v
 appium &
+
+# Wait for any process to exit
+wait -n
